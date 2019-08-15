0fc5811ab6393340eec075769a2e0e30d5f3d055
Justin Hartman <hartma62@purdue.edu>
2019-08-14 20:59:28 -0400

    website overhaul

--- js/ch-demo.js
+++ js/ch-demo.js
@@ -59,7 +59,7 @@ function sprite (options) {
 
         that.width = numberOfFrames * 64;
 
-    	that.context.clearRect(0,0,width,height);
+        that.context.clearRect(0,0,width,height);
         // Draw the animation
         that.context.drawImage(
 		    that.image,
@@ -70,16 +70,29 @@ function sprite (options) {
 		    player.x,
 		    player.y,
 		    that.width / numberOfFrames,
-		    that.height);
+            that.height);
+        
+        //console.log(obstacles);
+        that.context.fillStyle = "red";
+        //that.context.fillRect(window.width * .2 - 148, window.width * .2 + 100 , obstacles[0].width, obstacles[0].height );
+
+        //that.context.fillRect(window.width * .38, window.width * .2 + 152, obstacles[1].width - 45, obstacles[1].height  );
+
+        if(!player.isPressed) {
+            that.context.font = '15px Quicksand';
+            that.context.fillStyle = 'white';
+            that.context.fillText("Press the arrow keys!", player.x - 30, player.y);
+        }
     };
 
     return that;
 }
  
-var canvas = document.getElementById("canvas"),
+var canvas = document.getElementById("ch-canvas"),
+    chOffset = 68,
     ctx = canvas.getContext("2d"),
-    width = $(window).width(),
-    height = 230,
+    width = document.getElementById("ch-canvas").getBoundingClientRect().width,
+    height = document.getElementById("ch-canvas").getBoundingClientRect().height,
     player = {
     	sprite : sprite({
 		    context: canvas.getContext("2d"),
@@ -90,7 +103,7 @@ var canvas = document.getElementById("canvas"),
             startFrame: 2,
 			ticksPerFrame: 14
 		}),
-		x : width/2,
+		x : width * .75,
 		y : height - 5,
 		width : 64,
 		height : 64,
@@ -98,17 +111,21 @@ var canvas = document.getElementById("canvas"),
 		jumpSpeed: 7.5,
 		velX: 0,
 		velY: 0,
-		jumping: false
+        jumping: false,
+        isPressed: false
     },
+    obstacles = [],
     keys = [],
     friction = 0.75,
     gravity = 0.2;
- 
+
 canvas.width = width;
 canvas.height = height;
+
+console.log(canvas.width);
  
 function update(){
-	player.sprite.render();
+    player.sprite.render();
     player.sprite.update();
   // check keys
     if (keys[38] || keys[32]) {
@@ -117,26 +134,35 @@ function update(){
        player.jumping = true;
        player.velY = -player.jumpSpeed -Math.abs(player.velX * .03);
       }
+      player.isPressed = true;
     }
     if (keys[39]) {
         philImage.src = "./images/Phil/ChickenSheet.png"
         // right arrow
-        if (player.velX < 0) {
+        if(rightCollision()) {
+            player.velX = 0;
+        }
+        else if (player.velX < 0) {
         	player.velX += 1.25;
         }
         else if (player.velX < player.speed) {             
             player.velX += .25;         
-        }     
+        } 
+        player.isPressed = true;    
     }     
     else if (keys[37]) {         
-        // left arrow  
         philImage.src = "./images/Phil/ChickenSheet-flip.png"
-        if (player.velX > 0) {
+        // left arrow  
+        if(leftCollision()) {
+            player.velX = 0;
+        }
+        else if (player.velX > 0) {
         	player.velX -= 1.25;
         }       
         else if (player.velX > -player.speed) {
             player.velX -= .25;
         }
+        player.isPressed = true;
     }
     else {
     	if (player.velX > 0) {
@@ -151,11 +177,15 @@ function update(){
     		} 
     		player.velX += friction;
     	}
-    	
     }
- 
+
     player.velY += gravity;
- 
+
+    if(bottomCollision()) {
+        player.velY = 0;
+        player.jumping = false;
+    }
+
     player.x += player.velX;
     player.y += player.velY;
  
@@ -175,6 +205,79 @@ function update(){
  
   requestAnimationFrame(update);
 }
+
+function rightCollision() {
+    //window.width * .38, 238, obstacles[1].width - 45, obstacles[1].height 
+    //window.width * .2 - 148, 180, obstacles[0].width, obstacles[0].height
+    if(
+        player.y + player.height > window.width * .2 + 152 &&
+        player.x + player.width > window.width * .38 &&
+        player.x < window.width * .38 + obstacles[1].width - 45
+    ) {
+        player.x = window.width * .38 - player.width - 1;
+        console.log("right");
+        return true;
+    }
+    else if(
+        player.y + player.height > window.width * .2 + 100 &&
+        player.x + player.width > window.width * .2 - 148 &&
+        player.x < window.width * .2 - 148 + obstacles[0].width
+    )  {
+        player.x = window.width * .2 - 149 - player.width;
+        console.log("right2");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function leftCollision() {
+    if(
+        player.y + player.height > window.width * .2 + 152 &&
+        player.x + player.width > window.width * .38 &&
+        player.x < window.width * .38 + obstacles[1].width - 45
+    )
+    {
+        console.log("left");
+        player.x = window.width * .38 + obstacles[1].width - 44;
+        return true;
+    }
+    else if(
+        player.y + player.height > window.width * .2 + 100 &&
+        player.x + player.width > window.width * .2 - 148 &&
+        player.x < window.width * .2 - 148 + obstacles[0].width
+    ) {
+        console.log("left");
+        player.x = window.width * .2 - 148 + obstacles[0].width;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function bottomCollision() {
+    if(
+        player.y + player.height > window.width * .2 + 144 && 
+        player.x < window.width * .38 + obstacles[1].width - 45 &&
+        player.x + player.width > window.width * .38
+    ) {
+        player.y = window.width * .2 + 144 - player.height;
+        return true;
+    }
+    else if(
+        player.y + player.height > window.width * .2 + 90 && 
+        player.x + player.width > window.width * .2 - 148 &&
+        player.x < window.width * .2 - 148 + obstacles[0].width
+    ) {
+        player.y = window.width * .2 + 90 - player.height;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
  
 document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
@@ -187,5 +290,41 @@ document.body.addEventListener("keyup", function(e) {
 });
  
 window.addEventListener("load",function(){
+    obstacles[0] = document.getElementById("yard-yarn").querySelector("img").getBoundingClientRect();
+    obstacles[1] = document.getElementById("nort").querySelector("img").getBoundingClientRect();
+    this.console.log(obstacles);
     update();
+});
+
+$(window).resize(function(){
+    canvas.width = document.getElementById("ch-canvas").getBoundingClientRect().width;
+    canvas.height = document.getElementById("ch-canvas").getBoundingClientRect().height; 
+    ctx = canvas.getContext("2d");
+    width = document.getElementById("ch-canvas").getBoundingClientRect().width;
+    height = document.getElementById("ch-canvas").getBoundingClientRect().height;
+    player = {
+    	sprite : sprite({
+		    context: canvas.getContext("2d"),
+		    width:  2 * 64,
+		    height: 64,
+		    image: philImage,
+		    numberOfFrames: 2,
+            startFrame: 2,
+			ticksPerFrame: 14
+		}),
+		x : width * .75,
+		y : height - 5,
+		width : 64,
+		height : 64,
+		speed: 8,
+		jumpSpeed: 7.5,
+		velX: 0,
+		velY: 0,
+		jumping: false
+    };
+    keys = [];
+    friction = 0.75;
+    gravity = 0.2;
+    obstacles[0] = document.getElementById("yard-yarn").querySelector("img").getBoundingClientRect();
+    obstacles[1] = document.getElementById("nort").querySelector("img").getBoundingClientRect();
 });
--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	var page = "";
 	$(document).on('click', 'a', function(event){
 
 		    $('html, body').animate({
@@ -9,19 +10,66 @@ $(document).ready(function() {
 		});
 
 	$(document).ready(function() {
+		var unFixed = true;
 	    $('.menu').scrollToFixed({
 	    	marginTop: 30,
-	    	preFixed: function() { 
-				    $(".menu li").css("opacity", 0.1);
-
-				    $(".menu li").hover(function() {
-				        $(this).animate({opacity: 1.0}, 50);
-				    }, function() {
-				        $(this).animate({opacity: 0.1}, 50);
-				    });
+	    	fixed: function() { 
+				console.log("Pre");
+				$(".menu li").css("opacity", 0.1);
+				unFixed = false;
+				$(".menu li").hover(function() {
+					console.log("hover", unFixed);
+					if(!unFixed) {
+						$(this).animate({opacity: 1.0}, 50);
+					}
+				}, function() {
+					if(!unFixed) {
+						$(this).animate({opacity: 0.1}, 50);
+					}
+				});  
 	    	},
-	    	postFixed: function() { $(".menu li").css("opacity", 1);}
+	    	postFixed: function() { 
+				console.log("post", unFixed);
+				$(".menu li").css("opacity", 1);
+				unFixed = true;
+			}
 	    });
-
 	});
+
+	var pageIds = ["#page1", "#projects", "#games"];
+	var pages = [];
+	for(var i = 0; i < pageIds.length; i++) {
+		var newPage = {};
+		newPage["name"] = pageIds[i];
+		newPage["top"] = $(pageIds[i]).offset().top - 500;
+		newPage["bottom"] = newPage["top"] + $(pageIds[i]).outerHeight();
+		$(pageIds[i]).css({"opacity": 0});
+		pages.push(newPage);
+	}
+
+	var pageNumber = -1;
+
+	function testScroll(ev){
+		if(window.pageYOffset < pages[0]["top"] && pageNumber > -1) {
+			pageNumber = -1;
+			for(var i = 0; i < pageIds.length; i++) {
+				$(pageIds[i]).animate({opacity: 0});
+			}
+		}
+
+		for(var i = 0; i < pages.length; i++) {
+
+			if(window.pageYOffset > pages[i]["top"] 
+			&& window.pageYOffset < pages[i]["bottom"]
+			&& pageNumber !== i) {
+				$(pageIds[pageNumber]).animate({opacity: 0}, 1000, "swing")
+				pageNumber = i;
+				console.log(pages[i]["name"], pages[i]);
+				$(pageIds[i]).animate({opacity: 1}, 1000, "swing")
+				
+				break;
+			}
+		}
+	}
+	window.onscroll=testScroll
 })
